d2df8ee80d81a23ee19c15dc87128a30682c4e65
Geko66 <73885312+Geko66@users.noreply.github.com>
2022-05-12 17:42:34 +0200

    memory

--- Pipeline/Pipeline.srcs/sources_1/new/MEMORY.vhd
+++ Pipeline/Pipeline.srcs/sources_1/new/MEMORY.vhd
@@ -1,5 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_signed.all;
+use ieee.numeric_std.all;
 
 entity MEMORY is
     port(
@@ -8,8 +10,8 @@ entity MEMORY is
         memory_data: in std_logic_vector(31 downto 0);
         rw,stall_prev: out std_logic;
         decoded_instruction: in std_logic_vector(90 downto 0);
-        instruction_z: out std_logic_vector(90 downto 0));
-        
+        instruction_z: out std_logic_vector(90 downto 0);
+        bytes:out std_logic_vector(1 downto 0));
 end MEMORY;
 
 architecture Behavioral of MEMORY is
@@ -27,9 +29,33 @@ process(clk,reset)
         if(clk = '1' and clk'event) then 
             if(reset = '1') then 
                 instruction_z <= x"0000000000000000000000" & "000";
-            else 
-            --case OPCODE is 
-           -- when 
+            else if (stall='0') then 
+            case OPCODE is 
+            when"0000011" =>
+            memory_dir<=decoded_instruction(58 downto 27);
+                case SUBOPCODE is 
+                    when "000" => instruction_z <= decoded_instruction(90 downto 59)& std_logic_vector(resize(signed(memory_data(31 downto 24)), 32))&decoded_instruction(26 downto 0);
+                    when "001" =>instruction_z <= decoded_instruction(90 downto 59)& std_logic_vector(resize(signed(memory_data(31 downto 16)), 32))&decoded_instruction(26 downto 0);
+                    when "010" =>instruction_z <= decoded_instruction(90 downto 59)& memory_data(31 downto 0)&decoded_instruction(26 downto 0);  
+                    when "100" =>instruction_z <= decoded_instruction(90 downto 59)& x"000000"&memory_data(31 downto 24)&decoded_instruction(26 downto 0);  
+                    when "101" =>instruction_z <= decoded_instruction(90 downto 59)& x"0000"&memory_data(31 downto 16)&decoded_instruction(26 downto 0);
+                    when others => instruction_z <= decoded_instruction;  
+                end case;
+             when "0100011" =>
+                instruction_z<= decoded_instruction;
+                memory_dir<=decoded_instruction(58 downto 27);
+                case SUBOPCODE is 
+                    when "000" => memory_to_write<= x"000000"&decoded_instruction(67 downto 59);
+                    bytes<="00";
+                    when "001" =>memory_to_write<=x"0000"&decoded_instruction(74 downto 59);
+                    bytes<="01";
+                    when "010" =>memory_to_write<= decoded_instruction(90 downto 59);
+                    bytes<="10";
+                    when others => memory_to_write <=x"00000000";
+                    end case;
+              when others => instruction_z <= decoded_instruction;  
+              end case;     
+              else instruction_z<=decoded_instruction;
         end if;
        end if;
  end process;
